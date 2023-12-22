2c18d2f58927e8800d8b38778afc74ed09befc9e
Mai EFAT <132103597+mai-efat@users.noreply.github.com>
2023-12-22 22:52:46 +0200

    Update student_solution.sh

--- student_solution.sh
+++ student_solution.sh
@@ -1,1 +1,40 @@
 
+#!/bin/bash
+
+path=$1
+
+cd $path
+for file in `ls`;
+do
+if [ -f $file ];then
+extension=${file##*.}
+if [ -z $extension ];then
+mv $file "$path/misc"
+else
+  
+case "$file" in
+
+*.jpg|*.jpeg|*.png|*.gif)
+mkdir "$path/$extension"
+mv "$file"  "$path/$extension"
+echo "$file" 
+;;
+*.txt|*.pdf|*.doc|*.docx)
+mkdir "$path/$extension"
+mv "$file" "$path/$extension"
+echo "$file"
+;;
+*.mp4|*.avi)
+mkdir "$path/$extension"
+mv "$file" "$path/$extension"
+echo "$file"
+;;
+*)
+mkdir "$path/misc"
+mv "$file" "path/$extension"
+;;
+esac
+fi
+fi 
+
+done
